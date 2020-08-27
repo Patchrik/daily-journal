3a9ab74b9edc3e09acbc8e5a42d947d0cf38e34d
Parker Kelley <Pklly92@gmail.com>
2020-08-26 20:19:37 -0500

    finished the last part of the chapter 1 journal practice.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
-console.log('main.js is connected');
+import { useJournalEntries } from './JournalDataProvider.js';
+import { JournalEntryComponent } from './JournalEntry.js';
+import { EntryListComponent } from './JournalEntryList.js';
+
+EntryListComponent();
