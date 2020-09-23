9e2d2e7a3aab25c4c274d3ff6ccf6d9315070e40
Parker Kelley <Pklly92@gmail.com>
2020-09-23 11:04:50 -0500

    got the filter to work, need to add a render allbutton just for the UX.

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -8,11 +8,11 @@ import { useMoods } from './MoodDataProvider.js';
 const eventHub = document.querySelector('aside');
 
 export const JournalEntryComponent = (entry) => {
-  let entryIdMood;
-  if (parseInt(entry.mood.id) === parseInt(entry.moodId)) {
-    entryIdMood = entry.mood.mood;
-  }
-  return `
+	let entryIdMood;
+	if (parseInt(entry.mood.id) === parseInt(entry.moodId)) {
+		entryIdMood = entry.mood.mood;
+	}
+	return `
         <li>
         <div id="entry--${entry.id}" class="journalEntry">
             <h4>Title: ${entry.concept}</h4>
@@ -26,27 +26,44 @@ export const JournalEntryComponent = (entry) => {
 };
 
 addEventListener('click', (clickEvent) => {
-  if (clickEvent.target.id.startsWith('deleteNote--')) {
-    const [throwAwayTaco, id] = clickEvent.target.id.split('--');
+	if (clickEvent.target.id.startsWith('deleteNote--')) {
+		const [throwAwayTaco, id] = clickEvent.target.id.split('--');
 
-    deleteEntry(id)
-      .then(getEntries)
-      .then((_) => {
-        const updatedEntries = useEntries();
-        EntryListComponent(updatedEntries);
-      });
-  }
+		deleteEntry(id)
+			.then(getEntries)
+			.then((_) => {
+				const updatedEntries = useEntries();
+				EntryListComponent(updatedEntries);
+			});
+	}
 });
 
+// this is a function that will render all the moods that we have in our mood array - so if we ever expand the list
+// of moods we can easily render a big list.
 export const renderMoodRadioButtons = () => {
-  const moodRadioDomTarget = document.querySelector(
-    '.radio--button--container'
-  );
-  let moodsArray = useMoods().map((moodObj) => {
-    return `
+	const moodRadioDomTarget = document.querySelector(
+		'.radio--button--container'
+	);
+	let moodsArray = useMoods().map((moodObj) => {
+		return `
   		<input type="radio" id="mood-radio--${moodObj.id}" name="mood-select" value="${moodObj.id}">
 		  <label for="mood-radio--${moodObj.id}">${moodObj.mood}</label>
 		  `;
-  });
-  return (moodRadioDomTarget.innerHTML = moodsArray.join(' '));
+	});
+	return (moodRadioDomTarget.innerHTML = moodsArray.join(' '));
 };
+
+// This will be the event with the logic to make sure we render the sorted list of notes.
+addEventListener('click', (clickEvent) => {
+	if (clickEvent.target.id.startsWith('mood-radio--')) {
+		console.log('You clicked a radio button');
+		const [trash, selectedMoodid] = clickEvent.target.id.split('--');
+		console.log('this is trash', trash, 'this is the id', selectedMoodid);
+		debugger;
+		let filteredEntriesArray = useEntries().filter(
+			(entriesObj) => parseInt(entriesObj.moodId) === parseInt(selectedMoodid)
+		);
+		console.log('This will be a filtered array', filteredEntriesArray);
+		EntryListComponent(filteredEntriesArray);
+	}
+});
